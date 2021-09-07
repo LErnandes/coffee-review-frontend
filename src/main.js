81362882fe73828ccdc0ea5df80e46950132e994
Petrodon <luis78270@gmail.com>
2021-09-07 16:00:32 -0300

    Updating home page

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import titleMixin from './plugins/titleMixin';
 
-Vue.config.productionTip = false
+
+Vue.mixin(titleMixin);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <hello-world />
+  <v-container fluid fill-height>
+    <v-row justify="center">
+      <h1 class="text-center">
+        <span class="font-weight-light display-1">WELCOME TO</span>
+        <br>
+        <span class="font-weight-black display-3">COFFEE BLOG</span>
+      </h1>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: "Home",
+};
 </script>
